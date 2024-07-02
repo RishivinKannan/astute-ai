32f2833f3284c2d77b1383a9755d2ed5ac076eb7
RishivinKannan <rishivinkannanrrk003@gmail.com>
2024-07-02 11:31:50 +0530

    fix: height of the grid modified for mobile screen

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -3,7 +3,7 @@ const Grid = ({ height = "h-[50rem]" }) => {
     <div
       className={`${height} w-full bg-black bg-grid-white/[0.04]  relative flex items-center justify-center`}
     >
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
     </div>
   );
 };
--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
       <Eclipse className="z-10 -top-64 md:-top-[800px] left-1/2 -translate-x-1/2" />
       <Eclipse className="z-10 -bottom-56 md:-bottom-[650px] -left-1/2 " />
       <Eclipse className=" z-10 -bottom-56 md:-bottom-[650px] -right-1/2 " />
-      <Grid height="h-[35rem] md:h-[50rem]" />
+      <Grid height="h-[40rem] md:h-[50rem]" />
       <div className="container absolute inset-0 z-20 mx-auto pt-8  ">
         <TextGenerateEffect
           className="  text-white text-center font-bold md:font-normal text-lg sm:text-2xl md:text-3xl lg:text-5xl tracking-wider mb-8"
