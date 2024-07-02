0863fade65acab8d7b310933fea3bafd9508f669
RishivinKannan <rishivinkannanrrk003@gmail.com>
2024-07-02 15:39:36 +0530

    fix: animation slowed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,7 +59,7 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        shake: "shake 0.82s cubic-bezier(.36,.07,.19,.97) infinite",
+        shake: "shake 2.5s cubic-bezier(.36,.07,.19,.97) infinite",
       },
     },
   },
