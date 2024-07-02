b8784451c677ba423ef3b48dfca51651a3116a93
RishivinKannan <rishivinkannanrrk003@gmail.com>
2024-07-02 14:15:13 +0530

    feat: login button added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -34,7 +34,7 @@ const NavBar = () => {
           <Image width={42} height={42} src={"./logo.svg"} alt="LOGO" />
           <span className={mokoto.className}>STUTE</span>
         </div>
-        <ul className="hidden md:flex gap-16 items-center translate-x-2 ">
+        <ul className="hidden md:flex gap-16 items-center translate-x-10">
           {navs.map((nav) => (
             <li key={nav.name}>
               <Link
@@ -46,14 +46,21 @@ const NavBar = () => {
             </li>
           ))}
         </ul>
-        <Button className="hidden md:block">Get started</Button>
+        <div className="flex gap-8">
+          <button className="hidden md:block font-semibold hover:underline underline-offset-2">
+            SIGN IN
+          </button>
+          <Button className="hidden md:block !px-8 font-semibold ">
+            LOGIN
+          </Button>
+        </div>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <button className=" md:hidden">
               <AlignRightIcon className="w-8 h-8" />
             </button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56 bg-indigo-1000 outline-none border-none">
+          <DropdownMenuContent className="md:hidden w-56 bg-indigo-1000 outline-none border-none">
             <DropdownMenuGroup>
               {navs.map((nav) => (
                 <DropdownMenuItem key={nav.name}>
@@ -67,9 +74,14 @@ const NavBar = () => {
               ))}
             </DropdownMenuGroup>
             <DropdownMenuSeparator className="bg-slate-800" />
-            <DropdownMenuGroup>
+            <DropdownMenuGroup className="space-y-1">
               <DropdownMenuItem>
-                <Button className="w-full">Get started</Button>
+                <button className="font-semibold w-full hover:underline underline-offset-2">
+                  SIGN IN
+                </button>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Button className="w-full font-semibold">LOGIN</Button>
               </DropdownMenuItem>
             </DropdownMenuGroup>
           </DropdownMenuContent>
